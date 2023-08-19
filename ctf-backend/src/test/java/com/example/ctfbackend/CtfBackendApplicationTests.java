7a256ed1010f49160d2d633451febd7c493acd6d
PT3000 <t380125a@naver.com>
2023-08-20 00:50:08 +0900

    2023.08.20

---
+++ ctf-backend/src/test/java/com/example/ctfbackend/CtfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ctfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
